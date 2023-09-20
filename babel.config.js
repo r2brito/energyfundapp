ec1bb9404ce282abdf2bacbceff49414a39c2d2b
Rodrigo Brito <ramos.rbrito@gmail.com>
2023-09-20 12:55:20 -0300

    folders structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@routes': './src/routes',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
