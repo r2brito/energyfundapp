4eb4d4a517d49e60d210b8544d63515523f75cc5
Rodrigo Brito <ramos.rbrito@gmail.com>
2023-09-20 16:59:04 -0300

    screens trade end portifolio

--- src/screens/trade/trade.screen.tsx
+++ src/screens/trade/trade.screen.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
 import {ScrollView, View} from 'react-native';
+
+import {Header} from '@components/header';
+import {ThemedContainer} from '@components/themed-container';
+import {ThemedText} from '@components/themed-text';
+import {FontSize, FontWeight} from '../../styles';
+
 import styles from './trade.style';
 
 export function TradeScreen({navigation: {navigate}}) {
   return (
     <View style={styles.root}>
-      <ScrollView keyboardShouldPersistTaps="always" />
+      <ThemedContainer style={styles.content}>
+        <Header
+          titleAlignment="center"
+          leftComponent={<></>}
+          centerComponent={
+            <ThemedText
+              type="black"
+              style={{...FontSize.textSM, ...FontWeight.Sora.semiBold}}>
+              Trade
+            </ThemedText>
+          }
+          rightComponent={<></>}
+          transparent
+        />
+        <ScrollView keyboardShouldPersistTaps="always">
+          <View style={styles.padding}>
+            <ThemedText
+              type="black"
+              style={{
+                ...FontSize.textSM,
+                ...FontWeight.Sora.semiBold,
+                textAlign: 'justify',
+              }}>
+              TrLorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.ade
+            </ThemedText>
+          </View>
+        </ScrollView>
+      </ThemedContainer>
     </View>
   );
 }
--- src/screens/trade/trade.style.ts
+++ src/screens/trade/trade.style.ts
@@ -1,8 +1,18 @@
 import {StyleSheet} from 'react-native';
+import {useColors} from '@hooks/use-colors.hook';
+
+const colors = useColors();
 
 export default StyleSheet.create({
   root: {
     flexGrow: 1,
     backgroundColor: 'white',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  padding: {
+    paddingHorizontal: 16,
   },
 });
